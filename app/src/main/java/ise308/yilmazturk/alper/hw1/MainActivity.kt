83a8d38b7897e81c99801693c270983a3d1720c2
Alper <alperylr@gmail.com>
2020-10-30 23:02:35 +0300

    Initial commit

---
+++ app/src/main/java/ise308/yilmazturk/alper/hw1/MainActivity.kt
@@ -0,0 +1,12 @@
+package ise308.yilmazturk.alper.hw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
